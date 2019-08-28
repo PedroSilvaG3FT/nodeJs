829ae84e31f6ab334a807c4617a2cea5f16c6049
Pedro <pedro.silva-dev@hotmail.com>
2019-08-28 02:30:31 -0300

    Adicionando connection string do Banco de dados

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const server = express();
+
+mongoose.connect(
+  "mongodb+srv://pedro:pedro@cluster0-aaqcx.mongodb.net/tindev?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
 
 server.use(express.json());
 
