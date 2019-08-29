8d58d0e8ffd42abfdf43cac1e120e9fcad99485e
Pedro <pedro.silva-dev@hotmail.com>
2019-08-29 00:49:49 -0300

    GET de Instancias de usuarios recebidos

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -1,9 +1,20 @@
 const Dev = require("../models/Dev");
 
 module.exports = {
-  store(req, res) {
-    console.log(req.params.devId); //Acessar parametro que vem atraves da rota
+  async store(req, res) {
     console.log(req.headers.user); //Acessar Usuario que vem do Header JWT
+    console.log(req.params.devId); //Acessar parametro que vem atraves da rota
+
+    const { user } = req.headers;
+    const { devId } = req.params;
+
+    const loggedDev = await Dev.findById(user); //Usuario que irá dar L ike
+    const targetDev = await Dev.findById(devId); // Usuario que irá receber Like
+
+    if (!targetDev) {
+      //verifica que so usuario que vai receber existe
+      return res.status(400).json({ error: "Dev not exists" });
+    }
 
     return res.json({ ok: true });
   }
