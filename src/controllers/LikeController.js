c7411315444c0201145bc490d01e6603c9ecfde9
Pedro <pedro.silva-dev@hotmail.com>
2019-08-29 00:42:31 -0300

    Acessar informacoes do header

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,7 +2,8 @@ const Dev = require("../models/Dev");
 
 module.exports = {
   store(req, res) {
-    console.log(req.params.devId);
+    console.log(req.params.devId); //Acessar parametro que vem atraves da rota
+    console.log(req.headers.user); //Acessar Usuario que vem do Header JWT
 
     return res.json({ ok: true });
   }
