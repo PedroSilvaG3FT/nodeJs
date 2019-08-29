444aac98dd8535a68f9a6778faaadc4dd390a18f
Pedro <pedro.silva-dev@hotmail.com>
2019-08-29 00:58:42 -0300

    Verifica se o usuario deu Match (o usuario que recebe o like tem o outro em sua lista de likes)

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -16,6 +16,10 @@ module.exports = {
       return res.status(400).json({ error: "Dev not exists" });
     }
 
+    if (targetDev.likes.includes(loggedDev._id)) {
+      console.log("DEU MATCH !");
+    }
+
     loggedDev.likes.push(targetDev._id); //inclui no vetor o usuario que deu like
 
     await loggedDev.save(); // Salvar alteracoes feitas no usuario
